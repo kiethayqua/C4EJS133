292da664a44cd8f11f73ae0dcc031ed52b74ad71
kietnguyen99 <kiet.nguyen2@mservice.com.vn>
2022-11-15 19:17:07 +0700

    init lesson

---
+++ Lesson6/index.js
@@ -0,0 +1,27 @@
+// tạo biến age với giá trị tuổi của bạn
+
+// tạo biến name với giá trị là tên của bạn
+
+// tạo biến greeting với giá trị là "Hello, My name is <name>, <age> years old!", in ra kết quả
+
+// Kiểm tra trong biến greeting có tồn tại từ "Hello" hay không, không phân biệt chữ hoa chữ thường, in ra kết quả
+
+// Tạo biến sayHi thay thể "Hello" trong greeting thành "Hi", in ra màn hình
+
+/*
+Tạo biến mark = 9.7 và thực hiện:
+- Làm tròn thành 10 và in ra
+- Làm tròn về 9 và in ra
+*/
+
+// Tạo 1 biến lưu độ dài cạnh của hình vuông và tính diện tích hình vuông đó sau đó in ra màn hình
+
+/*
+Viết cấu trúc điều kiện kiểm tra độ tuổi lái xe với điều kiện sau: 
+- Nếu lớn hơn hoặc bằng 18 tuổi in ra màn hình: "Bạn đã đủ tuổi lái xe"
+- Nếu nhỏ hơn 18 tuổi in ra màn hình: "Bạn chưa đủ tuổi lái xe"
+*/
+
+/*
+Viết cấu trúc switch-case kiểm tra số chẳn và lẻ
+*/
