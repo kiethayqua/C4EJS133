3d7eb4a9478d979f09b622eed04b169d2ca927b1
kietnguyen99 <kiet.nguyen2@mservice.com.vn>
2022-11-19 19:00:14 +0700

    init lesson

---
+++ Lesson7/index.js
@@ -0,0 +1,38 @@
+// Bài 1: In ra màn hình các số từ 1..100 theo thứ tự tăng dần
+
+// Bài 2: In ra màn hình các số từ 1..100 theo thứ tự giảm dần
+
+// Bài 3: Cho số nguyên n bất kì, in các số chẵn từ 1..n
+
+// Bài 4: Cho số nguyên n bất kì, in các số lẻ từ 1..n
+
+// Bài 5: Cho số nguyên n bất kì, tính tổng các số từ 1..n
+
+// Bài 6: Cho số nguyên n. Tính giá trị biểu thức S= 1.2 + 2.3 + 3.4 + ... + n(n+1).
+
+// Bài 7: Cho các số nguyên n và m bất kì, tính n^m VD: 2^3 = 8
+
+// Bài 8: Cho số nguyên n bất kì, tính n! VD: 3! = 1*2*3 = 6
+
+// Bài 9: Vẽ tam giác có dạng sau:
+// *****
+// ****
+// ***
+// **
+// *
+
+// Bài 10: Xây dựng chương trình game đoán số từ 0..9(số được random)
+// - Cho phép nhập từ trình duyệt với nội dung - 'Nhập số: '
+// - Mỗi lần đoán sai, thông báo và cho nhập lại - 'Bạn đã đoán sai! Thử lại: '
+// - Trò chơi kết thúc khi đoán đúng số được random
+// - In ra kết qủa và số lần đã đoán
+// VD: 
+// Bạn đã đoán đúng!
+// Số được chọn là: <number>
+// Số lần bạn đoán là: <number>
+
+
+
+
+
+
