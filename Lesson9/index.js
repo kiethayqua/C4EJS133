efb23e4ab96c4a3d4460476f30a73b80b093abba
kietnguyen99 <kiet.nguyen2@mservice.com.vn>
2022-11-26 21:42:30 +0700

    example code

---
+++ Lesson9/index.js
@@ -0,0 +1,54 @@
+// const ITEMS = [
+//     {
+//         id: 0,
+//         title: 'I will wake up at 8 in the morning'
+//     },
+//     {
+//         id: 1,
+//         title: 'I will code at 9 in the morning'
+//     },
+//     {
+//         id: 2,
+//         title: 'I will eat at 12 in the morning'
+//     },
+//     {
+//         id: 3,
+//         title: 'I will sleep at 13 in the morning'
+//     }
+// ];
+
+// const list = document.getElementById('todo-list');
+
+// function createItemList(item) {
+
+//     // create img element
+//     const img = document.createElement('img');
+//     img.style.width = '24px';
+//     img.style.height = '24px';
+//     img.alt = 'icon';
+//     img.src = 'icons8-trash-can-48.png';
+
+//     // create img wrapper
+//     const imgWrapper = document.createElement('div');
+//     imgWrapper.classList.add('delete-btn', 'center');
+//     imgWrapper.appendChild(img);
+
+//     // create span 
+//     const title = document.createElement('span');
+//     title.innerHTML = item.title;
+
+//     // list item element
+//     const itemWrapper = document.createElement('li');
+//     itemWrapper.classList.add('todo-item');
+//     itemWrapper.appendChild(title);
+//     itemWrapper.appendChild(imgWrapper);
+
+//     list.appendChild(itemWrapper);
+// }
+
+// ITEMS.forEach(item => {
+//     createItemList(item);
+// });
+
+
+
