a409019b9655269ef5b70cc55dc28d6600349065
kietnguyen99 <kiet.nguyen2@mservice.com.vn>
2022-11-29 21:12:39 +0700

    update

--- Lesson10/index.js
+++ Lesson10/index.js
@@ -35,70 +35,70 @@ const ITEMS = [
         sold: 5900
     },
     {
-        id: 0,
+        id: 5,
         name: 'Son bbia 25 đỏ nâu nắp đỏ hàng handmade son kem lì không chì nắp đỏ',
         image: 'https://cf.shopee.vn/file/203eb8c2f22283fe10e491ee53793eb0_tn',
         price: 10000,
         sold: 797
     },
     {
-        id: 1,
+        id: 6,
         name: 'Áo thun Tealab Việt Nam , áo over size coton dày dặn- V7 Official ( V35)',
         image: 'https://cf.shopee.vn/file/e69694081b5d31ba9ccdd6b8c71f3c60_tn',
         price: 89000,
         sold: 6200
     },
     {
-        id: 2,
+        id: 7,
         name: 'GIẦY THỂ THAO ĐỘN ĐẾ 7 PHÂN TĂNG CHIỀU CAO(cách chọn size phần mô tả)',
         image: 'https://cf.shopee.vn/file/c46c7195a7a4e3665b95453e6cdafebf_tn',
         price: 119000,
         sold: 10300
     },
     {
-        id: 3,
+        id: 8,
         name: 'Bút Xoay Có Đèn LED Cho Học Sinh Bút quay',
         image: 'https://cf.shopee.vn/file/181063df8d932814c4a7c02b52b5ef6d_tn',
         price: 5000,
         sold: 1800
     },
     {
-        id: 4,
+        id: 9,
         name: 'Strap for Watch Band 42mm Elastic Sport Silicone Solo Loop bracelet YKD',
         image: 'https://cf.shopee.vn/file/0cbe3b95de7ba1b52fae8fae3ee5a394_tn',
         price: 5000,
         sold: 5900
     },
     {
-        id: 0,
+        id: 10,
         name: 'Son bbia 25 đỏ nâu nắp đỏ hàng handmade son kem lì không chì nắp đỏ',
         image: 'https://cf.shopee.vn/file/203eb8c2f22283fe10e491ee53793eb0_tn',
         price: 10000,
         sold: 797
     },
     {
-        id: 1,
+        id: 11,
         name: 'Áo thun Tealab Việt Nam , áo over size coton dày dặn- V7 Official ( V35)',
         image: 'https://cf.shopee.vn/file/e69694081b5d31ba9ccdd6b8c71f3c60_tn',
         price: 89000,
         sold: 6200
     },
     {
-        id: 2,
+        id: 12,
         name: 'GIẦY THỂ THAO ĐỘN ĐẾ 7 PHÂN TĂNG CHIỀU CAO(cách chọn size phần mô tả)',
         image: 'https://cf.shopee.vn/file/c46c7195a7a4e3665b95453e6cdafebf_tn',
         price: 119000,
         sold: 10300
     },
     {
-        id: 3,
+        id: 13,
         name: 'Bút Xoay Có Đèn LED Cho Học Sinh Bút quay',
         image: 'https://cf.shopee.vn/file/181063df8d932814c4a7c02b52b5ef6d_tn',
         price: 5000,
         sold: 1800
     },
     {
-        id: 4,
+        id: 14,
         name: 'Strap for Watch Band 42mm Elastic Sport Silicone Solo Loop bracelet YKD',
         image: 'https://cf.shopee.vn/file/0cbe3b95de7ba1b52fae8fae3ee5a394_tn',
         price: 5000,
@@ -125,7 +125,7 @@ function modifyPrice(price) {
 
 ITEMS.forEach((item) => {
     finalHTML += `
-    <div class="item">
+    <div class="item" onclick="logName(${item.id})">
         <img class="item-image" alt="image" src="${item.image}">
         <div class="item-content">
             <div class="item-name">${item.name}</div>
@@ -139,3 +139,20 @@ ITEMS.forEach((item) => {
 });
 
 list.innerHTML = finalHTML;
+
+const cartButton = document.getElementById('cart');
+const quantity = document.getElementById('quantity');
+let count = 0;
+let carts = [];
+
+function logName(id) {
+    const item = ITEMS.find((item) => `${item.id}` === `${id}`);
+    const index = carts.findIndex(i => i.id === id);
+    if (index !== -1) {
+        carts[index].quantity += 1;
+    } else {
+        carts.push({ ...item, quantity: 1 });
+    }
+
+    console.log(carts);
+}
